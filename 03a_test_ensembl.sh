9f02da02f668325e6c2f3e9d4a0fed4c239f6428
Sujai Kumar <sujaikumar@users.noreply.github.com>
2017-06-08 10:30:35 +0100

    Create 03a_test_ensembl.sh

--- 03a_test_ensembl.sh
+++ 03a_test_ensembl.sh
@@ -4,11 +4,17 @@
 ##==============================================================================
 
 ## WORKSHOP PARTICIPANTS DO THIS STEP BEFORE RUNNING THIS SCRIPT
-## Edit setup.ini config file, add SPECIES_DBS that you chose in database.ini
-## SPECIES_DBS = [ melitaea_cinxia_core_32_85_1 aedes_aegypti_core_32_85_3 ]
+## Edit setup.ini config file, add a new line to SPECIES_DBS
+## choose ensembl database name from
+## ftp://ftp.ensemblgenomes.org/pub/release-32/metazoa/mysql
+## eg:
+## SPECIES_DBS = [
+##   melitaea_cinxia_core_32_85_1
+##   aedes_aegypti_core_32_85_3
+## ]
 ## and comment out the 3 lines beginning GENOMEHUBS_PLUGIN_...
 
-# nano ~/genomehubs/v1/ensembl/conf/database.ini
+nano ~/genomehubs/v1/ensembl/conf/setup.ini
 
 ##------------------------------------------------------------------------------
 ## Run the `genomehubs/easy-mirror` Docker container to launch the site
